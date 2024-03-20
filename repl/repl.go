c29ff06912ada3a3783ae4bede4e4a4b5f1ed53f
rainke <woshihepeng520@163.com>
2024-03-20 14:00:30 +0800

    feat: repl output program string

--- repl/repl.go
+++ repl/repl.go
@@ -3,12 +3,21 @@ package repl
 import (
 	"bufio"
 	"cat/lexer"
-	"cat/token"
+	"cat/parser"
 	"fmt"
 	"io"
 )
 
 const PROMPT = ">> "
+
+const CatLogo = `
+  _______ _     _ _     _ _     _
+ |__   __| |   (_) |   | | |   | |
+    | |  | |__  _| | __| | | __| |
+    | |  | '_ \| | |/ _' | |/ _' |
+    | |  | | | | | | (_| | | (_| |
+    |_|  |_| |_|_|_|\__,_|_|\__,_|
+`
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
@@ -22,9 +31,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, CatLogo)
+	io.WriteString(out, "\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
