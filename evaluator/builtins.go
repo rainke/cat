c3c38981c777319f1416e00a060ec6b4cf3cdde6
rainke <woshihepeng520@163.com>
2024-04-15 13:56:56 +0800

    feat: 操作数组的内置函数

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -12,9 +12,54 @@ var builtins = map[string]*object.Builtin{
 			switch arg := args[0].(type) {
 			case *object.String:
 				return &object.Integer{Value: int64(len(arg.Value))}
+			case *object.Array:
+				return &object.Integer{Value: int64(len(arg.Elements))}
 			default:
 				return newError("argument to 'len' not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return newError("argument to 'first' must be ARRAY, got %s", args[0].Type())
+			}
+			if len(arr.Elements) == 0 {
+				return NULL
+			}
+			return arr.Elements[0]
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return newError("argument to 'last' must be ARRAY, got %s", args[0].Type())
+			}
+			if len(arr.Elements) == 0 {
+				return NULL
+			}
+			return arr.Elements[len(arr.Elements)-1]
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2", len(args))
+			}
+			arr, ok := args[0].(*object.Array)
+			if !ok {
+				return newError("argument to 'push' must be ARRAY, got %s", args[0].Type())
+			}
+			arr.Elements = append(arr.Elements, args[1])
+			return &object.Integer{Value: int64(len(arr.Elements))}
 		},
 	},
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -60,6 +60,7 @@ func TestBuiltInFunctions(t *testing.T) {
 		{`len("hello world")`, 11},
 		{`len(1)`, `argument to 'len' not supported, got INTEGER`},
 		{`len("hello", "world")`, `wrong number of arguments. got=2, want=1`},
+		{`len([1, 2, 3])`, 3},
 	}
 
 	for _, tt := range tests {
